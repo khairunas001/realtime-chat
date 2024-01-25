ce417908ab821bc30fce92212c55d043b24ac6ca
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2024-01-25 16:26:54 +0700

    percobaan pertama

---
+++ chatroom/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname, './public')));
+
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
